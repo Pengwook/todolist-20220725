91db28460a0cd5224b5dfb430727629be288e1af
LAPTOP-LJ90HDD8\khw12 <khw1204@naver.com>
2022-07-25 23:42:50 +0900

    todolist 복습 과정

---
+++ src/main/java/com/hyeonwook/todolist/service/todo/TodoService.java
@@ -0,0 +1,12 @@
+package com.hyeonwook.todolist.service.todo;
+
+import com.hyeonwook.todolist.web.dto.todo.CreateTodoReqDto;
+
+public interface TodoService {
+	//추가
+	public boolean createTodo(CreateTodoReqDto createTodoReqDto) throws Exception;
+	//수정
+	
+	//삭제
+	//조회
+}
